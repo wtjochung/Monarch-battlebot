2415dd76f02af4b58eb2833955ab45d3369cb62d
Jo Chung <chjoanna@163.com>
2022-11-26 04:14:55 -0500

    Fixed wheel turning error

--- BattleBot_TransmitterCode/BattleBot_TransmitterCode.ino
+++ BattleBot_TransmitterCode/BattleBot_TransmitterCode.ino
@@ -4,6 +4,7 @@ Adafruit_MotorShield MotorShield= Adafruit_MotorShield();
 Adafruit_DCMotor *wheel_l = MotorShield.getMotor(1);
 Adafruit_DCMotor *wheel_r = MotorShield.getMotor(2);
 Adafruit_DCMotor *wheel_weapon = MotorShield.getMotor(3);
+//Adafruit_DCMotor *test_motor = MotorShield.getMotor(4);
 
 #define CHANNEL1 11 //Right - horizontal
 #define CHANNEL2 10 //Right - vertical
@@ -19,16 +20,30 @@ int channel4Value;
 int channel5Value;
 int channel6Value;
 
-int readChannel(int channelInput, int mapLower, int mapUpper, int defaultValue){
-   int ch = pulseIn(channelInput, HIGH, 30000); //gets length of HIGH pulse, times out after 30000 miliseconds
-   if (ch < 100) return defaultValue;
-   return map(ch, 1000, 2000, mapLower, mapUpper);
+uint8_t max_speed;
+uint8_t wheel_speed;
+uint8_t turning_speed;
+uint8_t turning_cur;
+
+// Read the number of a specified channel and map the reading to the provided range
+// If the channel is off, return the default value
+long readChannel(int channelInput, int mapLower, int mapUpper, int defaultValue){
+  int ch = pulseIn(channelInput, HIGH, 30000); // Gets length of HIGH pulse, times out after 30000 useconds
+  if (ch < 100) return defaultValue; // If less than 100 microseconds, not a significant pulse
+  long mapped_value = map(ch, 1000, 2000, mapLower, mapUpper); // Map the value to the provided min/max
+  // this mapped value could end up being greater than 2000 useconds or less than 1000 useconds, hence these if cases
+  if (mapped_value < -255) 
+    return -255;
+  else if (mapped_value > 255) 
+    return 255;
+  else
+    return mapped_value; 
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Radio workshop part 1");
+  Serial.println("Monarch code running;");
   pinMode(CHANNEL1, INPUT);
   pinMode(CHANNEL2, INPUT);
   pinMode(CHANNEL3, INPUT);
@@ -37,9 +52,10 @@ void setup() {
   pinMode(CHANNEL6, INPUT);
 
   if (!MotorShield.begin()) {
-    Serial.println("Motor shield didn't boot correctly");
+    Serial.println("Motor shield didn't boot correctly :( Check wiring.");
+    while (1);
   }
-  Serial.println("Motor shield did boot correctly");
+  Serial.println("Motor shield did boot correctly :D");
 
   //Wheel start
   wheel_l->run(RELEASE);
@@ -59,23 +75,25 @@ void loop() {
   channel2Value = readChannel(CHANNEL2, -255, 255, 0);
   channel3Value = readChannel(CHANNEL3, -255, 255, 0);
   channel4Value = readChannel(CHANNEL4, -255, 255, 0);
-  channel5Value = readChannel(CHANNEL5, -255, 255, 0);
-  channel6Value = readChannel(CHANNEL6, -255, 255, 0);
+  //channel5Value = readChannel(CHANNEL5, -255, 255, 0);
+  //channel6Value = readChannel(CHANNEL6, -255, 255, 0);
 
   //radio test code
-  /*
+  
   Serial.print("Right stick horizontal: ");
   Serial.print(channel1Value);
   Serial.print(", ");
   Serial.print("Right stick vertical: ");
   Serial.print(channel2Value);
+  Serial.print(" ");
+  /*
   Serial.print(", ");
   Serial.print("Left stick vertical: ");
   Serial.print(channel3Value);
   Serial.print(", ");
   Serial.print("Left stick horizontal: ");
   Serial.print(channel4Value);
-  */
+  
   /*
   Serial.print(", ");
   Serial.print("VRA: ");
@@ -104,41 +122,51 @@ void loop() {
   */
 
 
+
   //Motor control
   //Note: uint8_t values only span from 0-255
-  uint8_t max_speed = 100;
-  uint8_t wheel_speed = abs(map(channel2Value, -260, 260, max_speed * -1, max_speed));
+  max_speed = 255;
+  wheel_speed = abs(map(channel2Value, -260, 260, max_speed * -1, max_speed));
+  //wheel_speed = readChannel(channel2Value, -255, 255, 0);
+  //wheel_speed = sqrt(channel2Value*channel2Value + horizontalValue*horizontalValue)/sqrt(2);
   
-  uint8_t turning_speed = 100;
+  turning_speed = 150;
   //TODO: make turning_cur go from 20-turning_speed
-  uint8_t turning_cur = turning_speed - (abs(map(channel1Value, -260, 260, turning_speed * -1, turning_speed)));
+  
+  turning_cur = turning_speed - (abs(map(channel1Value, -260, 260, turning_speed * -1, turning_speed)));
+  //turning_cur = readChannel(channel1Value, -255, 255, 0);
   
   int center_min = -20;
   int center_max = 20;
 
-  Serial.print("wheel_speed: ");
+  //test_motor->run(FORWARD);
+  //test_motor->setSpeed(wheel_speed);
+
+  /*
+  Serial.print("Wheel_speed: ");
   Serial.print(wheel_speed);
   Serial.print("; ");
 
   Serial.print("Wheel direction: ");
+  */
   if ((channel1Value >= center_min) && (channel1Value <= center_max)){//right horizontal: the center postion
-    Serial.print("center ");
+    //Serial.print("center ");
     if (channel2Value <= center_min){ //center backward
-          Serial.println("backward");
+          //Serial.println("backward");
           wheel_l->run(BACKWARD);
           wheel_r->run(BACKWARD);
       
           wheel_l->setSpeed(wheel_speed);
           wheel_r->setSpeed(wheel_speed);
     } else if (channel2Value >= center_max) { //center foward
-          Serial.println("forward");
+          //Serial.println("forward");
           wheel_l->run(FORWARD);
           wheel_r->run(FORWARD);
       
           wheel_l->setSpeed(wheel_speed);
           wheel_r->setSpeed(wheel_speed);
     } else { //center position. stop
-          Serial.println("stop");
+          //Serial.println("stop");
           wheel_l->run(FORWARD);
           wheel_r->run(FORWARD);
       
@@ -147,76 +175,83 @@ void loop() {
     }
     
   } else if (channel1Value > center_max){ //going right
-    Serial.print("right ");
+    //Serial.print("right ");
     if (channel2Value > center_max){ //going forward right
-      Serial.println("forward");
+           // Serial.println("forward");
       wheel_l->run(FORWARD);
+      wheel_r->setSpeed(turning_cur);
       wheel_r->run(FORWARD);
-      
       wheel_l->setSpeed(wheel_speed);
-      wheel_r->setSpeed(turning_cur);
     } else if (channel2Value < center_min) { //going backward right
-      Serial.println("backward");
+
+      //Serial.println("backward");
       wheel_l->run(BACKWARD);
-      wheel_r->run(BACKWARD);
-      
-      wheel_l->setSpeed(wheel_speed);
       wheel_r->setSpeed(turning_cur);
-    } else { //right position. stop
-      //right
-      Serial.println("stop");
-      wheel_l->run(RELEASE);
-      wheel_r->run(RELEASE);
+      wheel_r->run(BACKWARD);
+      wheel_l->setSpeed(wheel_speed);
+
       
-      wheel_l->setSpeed(0);
-      wheel_r->setSpeed(0);
+    } else { //right position. turn in place
+      //Serial.println("in-place");
+      uint8_t turning_speed_reverse = (abs(map(channel1Value, -260, 260, turning_speed * -1, turning_speed)));
+      wheel_l->run(FORWARD);
+      wheel_l->setSpeed(turning_speed_reverse);
+      
+      wheel_r->run(BACKWARD);
+      wheel_r->setSpeed(turning_speed_reverse);
     }
     
   } else if (channel1Value < center_min){ //going left
-    Serial.print("left ");
+    //Serial.print("left ");
     if (channel2Value > center_max){ //going forward left
-      Serial.println("forward");
+
+
+      //Serial.println("forward");
       wheel_l->run(FORWARD);
-      wheel_l->setSpeed(turning_cur);
       wheel_r->run(FORWARD);
+      
       wheel_r->setSpeed(wheel_speed);
-    } else if (channel2Value < center_min) { //going backward left
-      Serial.println("backward");
-      wheel_l->run(BACKWARD);
       wheel_l->setSpeed(turning_cur);
-      wheel_r->run(BACKWARD);
-      wheel_r->setSpeed(wheel_speed);
-    } else { //left position. stop.
-      //TODO
-      Serial.println("left");
+    } else if (channel2Value < center_min) { //going backward left
+
+
+      //Serial.println("backward");
       wheel_l->run(BACKWARD);
-      wheel_l->setSpeed(0);
       wheel_r->run(BACKWARD);
-      wheel_r->setSpeed(0);
+      
+      wheel_r->setSpeed(wheel_speed);
+      wheel_l->setSpeed(turning_cur);
+    } else { //left position. turn in place
+      //Serial.println("in-place");
+      uint8_t turning_speed_reverse = (abs(map(channel1Value, -260, 260, turning_speed * -1, turning_speed)));
+      wheel_l->run(BACKWARD);
+      wheel_l->setSpeed(turning_speed_reverse);
+      wheel_r->run(FORWARD);
+      wheel_r->setSpeed(turning_speed_reverse);
     }
   } 
 
   //Weapon control
   uint8_t max_weapon_speed = 250;
   uint8_t weapon_speed = abs(map(channel3Value, -260, 260, 0, max_weapon_speed));
-  if (channel3Value == 0) {
-    weapon_speed = 0;
-  } else {
-
+  //uint8_t weapon_speed = channel3Value;
+ 
   int weapon_start_threshold = -230;
   int weapon_reverse_threshold = 50;
- 
-  Serial.print("Weapon_speed: ");
+
+  Serial.print("Channel3value: ");
+  Serial.print(channel3Value);
+  Serial.print("; Weapon_speed: ");
   Serial.print(weapon_speed);
   Serial.print("; Direction: ");
 
   //Cases: low forward, high forward, low backward, high backward
-  if (channel3Value <= weapon_start_threshold){
+  if (channel3Value <= weapon_start_threshold || channel3Value == 0){
     //Do not start weapon motor
-    Serial.print("Weapon stopped");
+    Serial.println("weapon stopped");
     wheel_weapon->run(FORWARD);
     wheel_weapon->setSpeed(0);
-  } else if (channel4Value >= weapon_reverse_threshold){
+  } else if (channel4Value > weapon_reverse_threshold){
       Serial.println("backward");
       wheel_weapon->run(BACKWARD);
       wheel_weapon->setSpeed(weapon_speed);
@@ -226,4 +261,4 @@ void loop() {
       wheel_weapon->setSpeed(weapon_speed);
     }
   }
-  }
+  
